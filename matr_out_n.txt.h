b49f6d561cf1ccbe17348c0034ff7142cc517969
malkivska <v.a.malkivska@student.khai.edu>
2025-04-16 08:58:08 +0300

    Add files via upload

---
+++ matr_out_n.txt.h
@@ -0,0 +1,18 @@
+#ifndef TASKS_H
+#define TASKS_H
+
+// Декларації функцій відповідно до main.cpp
+
+// Завдання 1: Перестановка частини масиву
+void executeTask1();
+
+// Завдання 2: Пошук схожих стовпців у матриці
+void executeTask2();
+
+// Завдання 3: Сортування символів (реверсування рядка)
+void executeTask3();
+
+// Головне меню
+void showMenu();
+
+#endif // TASKS_H
